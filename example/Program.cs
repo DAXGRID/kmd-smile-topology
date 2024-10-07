a012e17b0d9145bb120f99f31b8efdc046165174
runeanielsen <runenielsen@runbox.com>
2024-10-07 17:47:39 +0200

    setup project with model and data

--- example/Program.cs
+++ example/Program.cs
@@ -1,10 +1,10 @@
-﻿
-namespace Example;
+﻿namespace Example;
 
 internal static class Program
 {
     public static void Main()
     {
-
+        var jsonExampleData = File.ReadAllText("/home/notation/projects/dax/kmd-smile-topology/example/data/example.json");
+        Console.WriteLine(jsonExampleData);
     }
 }
